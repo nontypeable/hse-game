bdd4b0eab7937f88a731c5f841faf76cccf06419
Andrey Sadkov <ad.sadkov@gmail.com>
2026-02-22 02:02:03 +0300

    feat(entity): make Entity drawable/transformable and add lifecycle, bounds, and draw hook
    - Make Entity inherit from sf::Drawable and sf::Transformable.

--- include/core/Entity.hpp
+++ include/core/Entity.hpp
@@ -2,13 +2,49 @@
 
 #include <SFML/Graphics.hpp>
 
-class Entity {
+class Entity : public sf::Drawable, public sf::Transformable {
 public:
-  virtual ~Entity() = default;
+  ~Entity() override = default;
 
   virtual void update(float dt) = 0;
-  virtual void render(sf::RenderTarget &target) const = 0;
-  virtual sf::Vector2f getPosition() const = 0;
 
-  virtual sf::FloatRect getBounds() const = 0;
+  virtual sf::FloatRect getLocalBounds() const = 0;
+  sf::FloatRect getGlobalBounds() const {
+    return this->getTransform().transformRect(this->getLocalBounds());
+  }
+
+  bool isAlive() const noexcept { return this->alive_; }
+  void markForRemoval() noexcept { this->alive_ = false; }
+
+  bool isActive() const noexcept { return this->active_; }
+  void setActive(bool active) noexcept { this->active_ = active; }
+
+  bool isVisible() const noexcept { return this->visible_; }
+  void setVisible(bool visible) noexcept { this->visible_ = visible; }
+
+  bool intersects(const Entity &other) const noexcept {
+    return this->getGlobalBounds()
+        .findIntersection(other.getGlobalBounds())
+        .has_value();
+  }
+  bool contains(sf::Vector2f point) const noexcept {
+    return this->getGlobalBounds().contains(point);
+  }
+
+protected:
+  void draw(sf::RenderTarget &target, sf::RenderStates states) const final {
+    if (!this->visible_ || !this->alive_) {
+      return;
+    }
+    states.transform *= this->getTransform();
+    this->onDraw(target, states);
+  }
+
+  virtual void onDraw(sf::RenderTarget &target,
+                      sf::RenderStates states) const = 0;
+
+private:
+  bool alive_ = true;
+  bool active_ = true;
+  bool visible_ = true;
 };
